e8095e3fc1dc6b5d2f8f713b8cca4276996d93c9
itn3000 <itn3000@gmail.com>
2019-12-19 18:03:47 +0900

    add native pkg(still WIP)

---
+++ gtool1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gtool1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
